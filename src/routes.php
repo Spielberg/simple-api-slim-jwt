ea082d4f5e04fff9dbae6252bf5ebbcfeb33ff56
Fco Javier Sanchez Ostiz Erviti <javi@Javier-MacBook-Pro.local>
2019-05-18 09:50:43 +0200

    use env files for configuration

--- src/routes.php
+++ src/routes.php
@@ -7,11 +7,29 @@ use Slim\Http\Response;
 return function (App $app) {
     $container = $app->getContainer();
 
-    $app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
-        // Sample log message
-        $container->get('logger')->info("Slim-Skeleton '/' route");
-
-        // Render index view
-        return $container->get('renderer')->render($response, 'index.phtml', $args);
-    });
+    $app->post('/login', function (Request $request, Response $response, array $args) {
+ 
+      $input = $request->getParsedBody();
+      $sql = "SELECT * FROM users WHERE email= :email";
+      $sth = $this->db->prepare($sql);
+      $sth->bindParam("email", $input['email']);
+      $sth->execute();
+      $user = $sth->fetchObject();
+   
+      // verify email address.
+      if(!$user) {
+          return $this->response->withJson(['error' => true, 'message' => 'These credentials do not match our records.']);  
+      }
+   
+      // verify password.
+      if (!password_verify($input['password'],$user->password)) {
+          return $this->response->withJson(['error' => true, 'message' => 'These credentials do not match our records.']);  
+      }
+   
+      $settings = $this->get('settings'); // get settings array.
+      
+      $token = JWT::encode(['id' => $user->id, 'email' => $user->email], $settings['jwt']['secret'], "HS256");
+   
+      return $this->response->withJson(['token' => $token]);
+  });
 };
--- src/settings.php
+++ src/settings.php
@@ -1,8 +1,20 @@
 <?php
+$dotenv = Dotenv\Dotenv::create(__DIR__.'/../');
+$dotenv->load();
+$dotenv->required(['JWT_SECRET']);
+
 return [
     'settings' => [
         'displayErrorDetails' => true, // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+
+        // Database
+        'db' => [            
+          'host' => '127.0.0.1',             
+          'dbname' => '002_api_andia',             
+          'user' => 'root',            
+          'pass' => 'root'        
+        ],
 
         // Renderer settings
         'renderer' => [
@@ -15,5 +27,10 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+
+        // jwt settings
+        'jwt' => [
+          'secret' => getenv('JWT_SECRET')
+      ]
     ],
 ];
