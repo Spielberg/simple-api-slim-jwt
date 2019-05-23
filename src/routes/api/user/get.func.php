ba3853e4f07d5036fbefac7e3c41915495da15d5
javi <fsanchez@tweetbinder.com>
2019-05-23 18:58:36 +0200

    put de usuarios

--- src/routes/api/user/get.func.php
+++ src/routes/api/user/get.func.php
@@ -19,25 +19,43 @@ return function (Request $request, Response $response, array $args) {
   // get params or set default.
   $limit = (int) $request->getQueryParam('limit', $settings['pagination']['limit']);
   $offset = (int) $request->getQueryParam('offset', 0);
+  $id = $request->getQueryParam('id', null);
+  $query = $request->getQueryParam('query', null);
 
   // get users detaills
-  $sql = 'SELECT id, name, email, created_at, last_login, active, superuser FROM users WHERE deleted = 0 LIMIT :limit OFFSET :offset';
-  $sth = $this->db->prepare($sql);
-  $sth->bindParam('limit', $limit, PDO::PARAM_INT);
-  $sth->bindParam('offset', $offset, PDO::PARAM_INT);
+  $params = [
+    [ 'key' => 'limit', 'var' => $limit, 'code' => PDO::PARAM_INT ],
+    [ 'key' => 'offset', 'var' => $offset, 'code' => PDO::PARAM_INT ],
+  ];
+  $select = 'SELECT id, name, email, created_at, last_login, active, superuser FROM users WHERE deleted = 0 ';
+  $count = 'SELECT count(*) FROM users WHERE deleted = 0 ';
+  if ($id !== null && $id !== '') {
+    $select .= "AND id = :id ";
+    $count  .= 'AND id = ' . $id;
+    $params[] = [ 'key' => 'id', 'var' => $id, 'code' => PDO::PARAM_INT ];
+  }
+  if ($query !== null && $query !== '') {
+    $select .= 'AND ( name LIKE :query OR email LIKE :query) ';
+    $count  .= 'AND ( name LIKE "%' . $query . '%" OR email LIKE "%' . $query . '%") ';
+    $params[] = [ 'key' => 'query', 'var' => '%' . $query . '%', 'code' => PDO::PARAM_STR ];
+  }
+  $select .= 'LIMIT :limit OFFSET :offset';
+  $sth = $this->db->prepare($select);
+  foreach($params as $obj) {
+    $sth->bindParam($obj['key'], $obj['var'], $obj['code']);
+  }
   try {
     $sth->execute();
+    $total = (int) $this->db->query($count)->fetchColumn();
   } catch(Exception $e) {
     return $this->response->withJson(['error' => true, 'message' => $e->getMessage()]);  
   }
-  $total = (int) $this->db->query('SELECT FOUND_ROWS()')->fetchColumn();
   $results = array_map(function ($result) {
     $result['active'] = (bool) $result['active'] == 1;
     $result['superuser'] = (bool) $result['superuser'] == 1;
     return $result;
   }, $sth->fetchAll());
-
-    
+  
   return $this->response->withJson([
     'error' => false,
     'data' => [
--- src/routes/api/user/put.func.php
+++ src/routes/api/user/put.func.php
@@ -27,15 +27,16 @@ return function (Request $request, Response $response, array $args) {
   ];
   if ($token['superuser']) {
     $allowed['active'] = PDO::PARAM_BOOL;
+    $allowed['superuser'] = PDO::PARAM_BOOL;
     $allowed['deleted'] = PDO::PARAM_BOOL;
   }
 
   $arrQuery = [];
   $arrValue = [];
-  foreach($allowed as $key => $value){
-    if (isset($input[$key]) && $input[$key] !== '') {
-      $arrQuery[] = $key;
-      $arrValue[] = "$key = :$key";
+  foreach($allowed as $w => $a){
+    if (isset($input[$w]) && $input[$w] !== '') {
+      $arrQuery[] = $w;
+      $arrValue[] = "$w = :$w";
     }
   }
   if(count($arrQuery) === 0) {
@@ -45,21 +46,21 @@ return function (Request $request, Response $response, array $args) {
   // build quey.
   $sql = 'UPDATE users SET ' . implode($arrValue, ', ') . ' WHERE id = :id LIMIT 1';
   $sth = $this->db->prepare($sql);
-  $sth->bindParam('id', $input['id']);
-  foreach($arrQuery as $key) {
-    $value = $key === 'password'
-      ? password_hash($input[$key], PASSWORD_DEFAULT)
-      : $input[$key];
-    $sth->bindParam($key, $value, $allowed[$key]);
+  $sth->bindParam('id', $input['id'], PDO::PARAM_INT);
+  foreach($arrQuery as $x) {
+    echo $x . "<br />";
+    $val = $x === 'password'
+      ? password_hash($input[$x], PASSWORD_DEFAULT)
+      : $input[$x];
+    //echo $val . "<br />";
+    $sth->bindParam($x, $val, $allowed[$x]);
   };
-  
+  //die ();
   try {
     $sth->execute();
   } catch(Exception $e) {
     return $this->response->withJson(['error' => true, 'message' => $e->getMessage()]);  
   }
-      
-  return $this->response->withJson([
-    'error' => false,
-  ]);
+     
+  return $this->response->withJson(['error' => false]);
 };
